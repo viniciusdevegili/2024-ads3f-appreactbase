250729f1ef0727faccfc866d5e3fed54297c692f
vinicius.dev <72517355+viniciusdevegili@users.noreply.github.com>
2024-03-24 16:18:45 -0300

    Etapa final

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,32 +1,22 @@
-import { Container, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 export default function Header() {
   return (
-    <Navbar expand={"lg"}>
+    <Navbar style={{ backgroundColor: '#4285f4' }} expand="lg">
       <Container>
-        <Navbar.Brand as={Link}>Juarema App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" style={{ marginLeft: '-230px' }}>Juarema App</Navbar.Brand>
         <Navbar.Toggle aria-controls="minhanav" />
-        <Navbar.Collapse
-          id="minhanav"
-          style={{
-            justifyContent: "space-between",
-            width: "100%",
-            alignSelf: "stretch",
-          }}
-        >
-          <Navbar.Text as={Link} to="/">
-            Inicial
-          </Navbar.Text>
-          <Navbar.Text as={Link} to="/sobre">
-            Sobre
-          </Navbar.Text>
-          <Navbar.Text as={Link} to="/contato">
-            Contato
-          </Navbar.Text>
-          <Navbar.Text as={Link} to="/albums">
-            Albums
-            </Navbar.Text>
+        <Navbar.Collapse id="minhanav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Inicial</Nav.Link>
+            <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+            <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
+            <Nav.Link as={Link} to="/albums">Albums</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/pages/Albums.jsx
+++ src/pages/Albums.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import album1 from '../images/drake.jpg';
+import album2 from '../images/mcig.png';
+import album3 from '../images/acdc.jpg';
+import album4 from '../images/travis.jpg';
+import album5 from '../images/21savage.jpg';
+import album6 from '../images/drake2.jpg';
 
 const cardStyle = {
     width: '18rem',
@@ -21,55 +26,73 @@ export default function Albums() {
                     <Card.Text>
                         Certified Lover Boy
                     </Card.Text>
-                    <Button variant="primary">Ver Álbum</Button>
+                    <Card.Text>
+                    Certified Lover Boy é o sexto álbum de estúdio do rapper canadense Drake, lançado em 3 de setembro de 2021 pela OVO Sound e Republic Records.
+                    </Card.Text>
+                    <Button variant="primary" as="a" href="https://www.youtube.com/playlist?list=PLxA687tYuMWibWyB256DEaHLL0ueEKL97" target="_blank">Ver Álbum</Button>
                 </Card.Body>
             </Card>
             <Card style={cardStyle}>
-                <Card.Img variant="top" src={album1} alt="Capa do Álbum" />
+                <Card.Img variant="top" src={album2} alt="Capa do Álbum" />
                 <Card.Body>
-                    <Card.Title>Drake</Card.Title>
+                    <Card.Title>Mc IG</Card.Title>
                     <Card.Text>
-                        Certified Lover Boy
+                    Todo Mundo Odeia o IG
+                    </Card.Text>
+                    <Card.Text>
+                        Todo mundo odeia o IG foi um album lançado pelo cantor brasileiro Mc Ig, lançado em 22 de março de 2024 pela Warner Music Brasil.
                     </Card.Text>
                     <Button variant="primary">Ver Álbum</Button>
                 </Card.Body>
             </Card>
             <Card style={cardStyle}>
-                <Card.Img variant="top" src={album1} alt="Capa do Álbum" />
+                <Card.Img variant="top" src={album3} alt="Capa do Álbum" />
                 <Card.Body>
-                    <Card.Title>Drake</Card.Title>
+                    <Card.Title>ACDC</Card.Title>
                     <Card.Text>
-                        Certified Lover Boy
+                        Back in black
+                    </Card.Text>
+                    <Card.Text>
+                    Back In Black é o sétimo álbum de estúdio da banda australiana de hard rock AC/DC, lançado à 25 de Julho de 1980, 5 meses depois do falecimento do vocalista.
                     </Card.Text>
                     <Button variant="primary">Ver Álbum</Button>
                 </Card.Body>
             </Card>
             <Card style={cardStyle}>
-                <Card.Img variant="top" src={album1} alt="Capa do Álbum" />
+                <Card.Img variant="top" src={album4} alt="Capa do Álbum" />
                 <Card.Body>
-                    <Card.Title>Drake</Card.Title>
+                    <Card.Title>Travis Scott</Card.Title>
                     <Card.Text>
-                        Certified Lover Boy
+                        Utopia
+                    </Card.Text>
+                    <Card.Text>
+                    Utopia é o quarto álbum de estúdio do rapper norte-americano Travis Scott. Seu lançamento ocorreu em 28 de julho de 2023, através da Cactus Jack e da Epic Records.
                     </Card.Text>
                     <Button variant="primary">Ver Álbum</Button>
                 </Card.Body>
             </Card>
             <Card style={cardStyle}>
-                <Card.Img variant="top" src={album1} alt="Capa do Álbum" />
+                <Card.Img variant="top" src={album5} alt="Capa do Álbum" />
                 <Card.Body>
-                    <Card.Title>Drake</Card.Title>
+                    <Card.Title>21 Savage</Card.Title>
                     <Card.Text>
-                        Certified Lover Boy
+                        American Dream
+                    </Card.Text>
+                    <Card.Text>
+                    American Dream é o terceiro álbum solo de estúdio e álbum de trilha sonora de estreia do rapper 21 Savage. 
                     </Card.Text>
                     <Button variant="primary">Ver Álbum</Button>
                 </Card.Body>
             </Card>
             <Card style={cardStyle}>
-                <Card.Img variant="top" src={album1} alt="Capa do Álbum" />
+                <Card.Img variant="top" src={album6} alt="Capa do Álbum" />
                 <Card.Body>
                     <Card.Title>Drake</Card.Title>
                     <Card.Text>
-                        Certified Lover Boy
+                        Her Loss
+                    </Card.Text>
+                    <Card.Text>
+                    Her Loss é um álbum de estúdio colaborativo entre o rapper canadense Drake e o rapper 21 Savage. 
                     </Card.Text>
                     <Button variant="primary">Ver Álbum</Button>
                 </Card.Body>
